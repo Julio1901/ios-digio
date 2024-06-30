4300c97c4f16f3322efac51785f49331da299d83
Julio Cesar <js.cesar42@gmail.com>
2024-06-30 14:17:48 -0300

    style: Skeleton Loading implementd

--- Digio/Presentation/Components/SpotlightCard.swift
+++ Digio/Presentation/Components/SpotlightCard.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class SpotlightCard: UIView {
+    let gradientLayer = CAGradientLayer()
+    
     override init(frame: CGRect) {
             super.init(frame: frame)
             setupCardView()
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = self.bounds
     }
     var image : UIImageView = {
         let it = UIImageView()
@@ -34,10 +40,35 @@ class SpotlightCard: UIView {
         self.layer.shadowOffset = CGSize(width: 0, height: 2)
         self.layer.shadowOpacity = 0.2
         self.layer.shadowRadius = 4
-        self.addSubview(image)
+        //self.addSubview(image)
+        setupSkeletonView()
     }
     private func setupConstraints() {
         self.heightAnchor.constraint(equalToConstant: 150).isActive = true
         self.widthAnchor.constraint(equalToConstant: 300).isActive = true
+    }
+    private func setupSkeletonView() {
+       
+        self.backgroundColor = UIColor.lightGray.withAlphaComponent(0.5)
+        self.layer.cornerRadius = 10
+        
+        gradientLayer.startPoint = CGPoint(x: 0.2, y: 1)
+        gradientLayer.endPoint = CGPoint(x: 1.2, y: 0)
+        gradientLayer.colors = [
+            UIColor.lightGray.withAlphaComponent(0.5).cgColor,
+            UIColor.white.withAlphaComponent(0.5).cgColor,
+            UIColor.lightGray.withAlphaComponent(0.5).cgColor
+        ]
+        gradientLayer.locations = [0, 0.5, 1]
+        gradientLayer.frame = self.bounds
+        gradientLayer.cornerRadius = 10
+        self.layer.addSublayer(gradientLayer)
+        
+        let animation = CABasicAnimation(keyPath: "locations")
+        animation.fromValue = [0, 0, 0.25]
+        animation.toValue = [0.75, 1, 1]
+        animation.duration = 1.5
+        animation.repeatCount = .infinity
+        gradientLayer.add(animation, forKey: "shimmer")
     }
 }
